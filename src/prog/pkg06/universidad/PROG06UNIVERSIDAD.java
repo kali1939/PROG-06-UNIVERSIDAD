45892df9f448ce30f8b1c89eb6ea7cd4b56583db
Sergio <smartines303@gmail.com>
2022-05-01 21:32:22 -0500

    Primera version

---
+++ src/prog/pkg06/universidad/PROG06UNIVERSIDAD.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prog.pkg06.universidad;
+
+/**
+ *
+ * @author Sergio
+ */
+public class PROG06UNIVERSIDAD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
